44d307a9e4d3f303ac0d335256b4d35f1aa9eaa2
Saurabh-Chede <saurabhchede21@gmail.com>
2026-07-16 11:57:48 +0530

    ...

--- Dsaday1.js
+++ Dsaday1.js
@@ -1,4 +1,4 @@
-let Input = [1, 0, 2, 5, 6, 7, 8, 10, 30, 12];
+let Input = [1, 0, 2, 5, 4, 9, 3, 3, 6, 7, 8, 10, 30, 12];
 
 function findSecondLargestN(arr) {
   let largest = -Infinity;
@@ -59,8 +59,90 @@ function findSecondSmallest(arr) {
       secondSmallest = arr[i];
     }
   }
-  return secondSmallest
+  return secondSmallest;
 }
 
 // console.log(findSecondSmallest(Input));
 
+function ReverseArray(arr) {
+  let reverse = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reverse.push(arr[i]);
+  }
+
+  return reverse;
+}
+
+console.log(ReverseArray(Input));
+
+function checkSortedArray(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+
+  return -1;
+}
+// console.log(checkSortedArray(Input));
+
+function countFrequency(arr) {
+  let frequency = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    if (frequency[arr[i]]) {
+      frequency[arr[i]]++;
+    } else {
+      frequency[arr[i]] = 1;
+    }
+  }
+
+  return frequency;
+}
+
+// console.log(countFrequency(Input));
+
+function findDuplicates(arr) {
+  let frequency = {};
+  let duplicates = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (frequency[arr[i]]) {
+      frequency[arr[i]]++;
+
+      if (frequency[arr[i]] === 2) {
+        duplicates.push(arr[i]);
+      }
+    } else {
+      frequency[arr[i]] = 1;
+    }
+  }
+
+  return duplicates;
+}
+
+console.log(findDuplicates(Input));
+
+// First non-repeating element
+
+function nonRepeatEL(arr) {
+  let frequency = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    if (frequency[arr[i]]) {
+      frequency[arr[i]]++;
+    } else {
+      frequency[arr[i]] = 1;
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (frequency[arr[i]] === 1) {
+      return arr[i];
+    }
+  }
+
+  return -1;
+}
+
+console.log(nonRepeatEL(Input));
