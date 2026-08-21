a221a205188d10c23e1c292c0d0c7676f27ba270
Saurabh-Chede <saurabhchede21@gmail.com>
2026-08-21 20:38:09 +0530

    ...

--- practice.js
+++ practice.js
@@ -1,6 +1,14 @@
-function addNumabers (a,b){
- return a + b
+function findSecondSmallest(arr) {
+  let smallest = Infinity;
+  let secondSmallest = Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      secondSmallest = smallest;
+      smallest = arr[i];
+    } else if (arr[i] < secondSmallest && arr[i] !== smallest) {
+      secondSmallest = arr[i];
+    }
+  }
+  return secondSmallest;
 }
-
-
-
