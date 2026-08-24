d7a017ae08c284618b36dc005b090407d40451e4
Saurabh-Chede <saurabhchede21@gmail.com>
2026-08-24 20:15:49 +0530

    dsa

--- practice.js
+++ practice.js
@@ -1,14 +1,66 @@
-function findSecondSmallest(arr) {
-  let smallest = Infinity;
-  let secondSmallest = Infinity;
+// closure
+
+function Parent() {
+  let money = 10000;
+
+  return function child() {
+    // console.log(money);
+  };
+}
+
+// let myfn = Parent()
+// myfn()
+
+// async
+
+// console.log(`start`);
+
+// console.log(`end`);
+
+// fetch('https://jsonplaceholder.typicode.com/posts/1')
+// .then(response => response.json())
+// .then(data => console.log(data));
+
+async function fetchpost() {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/1",
+    );
+    if (!response.ok) {
+      throw new Error(`error fetching response`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+}
+
+// fetchpost();
+
+function findLargestElemnt(arr) {
+  let largest = -Infinity;
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < smallest) {
-      secondSmallest = smallest;
-      smallest = arr[i];
-    } else if (arr[i] < secondSmallest && arr[i] !== smallest) {
-      secondSmallest = arr[i];
+    if (arr[i] > largest) {
+      largest = arr[i];
     }
   }
-  return secondSmallest;
+
+  return largest;
 }
+
+console.log(findLargestElemnt([1, 2, 10, 40, 5]));
+
+function ReverseArray(arr) {
+  let result = [];
+  for (let i = arr.length-1; i >= 0; i--) {
+    result.push(arr[i]);
+  }
+  return result
+}
+
+console.log(ReverseArray([1,2,3,4,5,6]));
+
+
+
